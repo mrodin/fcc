33ec17aad1eb53b685478ebd2baae84c0cb759d7
Martin Rodin <rodin.martin@gmail.com>
2017-05-09 15:16:04 +0200

    add quotes JSON

--- random-quote-machine/scripts/script-main.js
+++ random-quote-machine/scripts/script-main.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
-    
+var quotesHTMLCode = "<i class=\"fa fa-quote-left\" aria-hidden=\"true\"></i>";
+
+$(document).ready(function () {
+    $('[name="new-quote"]').click(function () {
+        $(".quote-text").html(quotesHTMLCode + "test");
+    });
 });
