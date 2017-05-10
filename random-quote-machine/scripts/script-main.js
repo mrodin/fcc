7e509f4298982382a5226227ac0a96f427b9917d
Martin Rodin <rodin.martin@gmail.com>
2017-05-10 22:57:17 +0200

    add getJSON method

--- random-quote-machine/scripts/script-main.js
+++ random-quote-machine/scripts/script-main.js
@@ -1,6 +1,10 @@
 var quotesHTMLCode = "<i class=\"fa fa-quote-left\" aria-hidden=\"true\"></i>";
 
 $(document).ready(function () {
+    $.getJSON("https://jsonblob.com/d82d9345-34c0-11e7-ae4c-6beae9fe30f9", function (data) {
+        
+    });
+
     $('[name="new-quote"]').click(function () {
         $(".quote-text").html(quotesHTMLCode + "test");
     });
