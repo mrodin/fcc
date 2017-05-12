92e451ba0396818daad01b4cdde1f8e90b959ab5
Martin Rodin <rodin.martin@gmail.com>
2017-05-12 16:43:26 +0200

    add functionality to show random quote

--- random-quote-machine/scripts/script-main.js
+++ random-quote-machine/scripts/script-main.js
@@ -1,11 +1,22 @@
-var quotesHTMLCode = "<i class=\"fa fa-quote-left\" aria-hidden=\"true\"></i>";
+var quotesHtmlCode = "<i class=\"fa fa-quote-left\" aria-hidden=\"true\"></i>";
 
 $(document).ready(function () {
-    $.getJSON("https://jsonblob.com/d82d9345-34c0-11e7-ae4c-6beae9fe30f9", function (data) {
-        
-    });
+    showRandomQuote();
 
-    $('[name="new-quote"]').click(function () {
-        $(".quote-text").html(quotesHTMLCode + "test");
+    $("[name=\"new-quote\"]").click(function () {
+        showRandomQuote();
     });
 });
+
+function showRandomQuote() {
+    var randomNum = Math.floor((Math.random() * 20));
+    $.getJSON("https://jsonblob.com/api/jsonBlob/d82d9345-34c0-11e7-ae4c-6beae9fe30f9", function (data) {
+        $(".quote-text").fadeOut(function() {
+            $(this).html(quotesHtmlCode + data.quotes[randomNum].text).fadeIn();
+        });
+        
+        $(".author").fadeOut(function() {
+            $(this).html("- " + data.quotes[randomNum].author).fadeIn();
+        });
+    });
+};
