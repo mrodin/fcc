a076ed7ada4e7b8f8d7ae07e3d7a06335ad9b2e8
Martin Rodin <rodin.martin@gmail.com>
2017-05-14 22:15:45 +0200

    fadeOut and fadeIn tweaks

--- random-quote-machine/scripts/script-main.js
+++ random-quote-machine/scripts/script-main.js
@@ -11,12 +11,12 @@ $(document).ready(function () {
 function showRandomQuote() {
     var randomNum = Math.floor((Math.random() * 20));
     $.getJSON("https://jsonblob.com/api/jsonBlob/d82d9345-34c0-11e7-ae4c-6beae9fe30f9", function (data) {
-        $(".quote-text").fadeOut(function() {
-            $(this).html(quotesHtmlCode + data.quotes[randomNum].text).fadeIn();
+        $(".quote-text").fadeOut("slow", function() {
+            $(this).html(quotesHtmlCode + data.quotes[randomNum].text).fadeIn("slow");
         });
         
-        $(".author").fadeOut(function() {
-            $(this).html("- " + data.quotes[randomNum].author).fadeIn();
+        $(".author").fadeOut("slow", function() {
+            $(this).html("- " + data.quotes[randomNum].author).fadeIn("slow");
         });
     });
 };
