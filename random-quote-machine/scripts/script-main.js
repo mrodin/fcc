9b19885ec2bfe83a929494d2d40cf8473cfb120d
Martin Rodin <rodin.martin@gmail.com>
2017-05-14 23:15:51 +0200

    add shareOnTwitter functionality

--- random-quote-machine/scripts/script-main.js
+++ random-quote-machine/scripts/script-main.js
@@ -1,10 +1,15 @@
-var quotesHtmlCode = "<i class=\"fa fa-quote-left\" aria-hidden=\"true\"></i>";
-
 $(document).ready(function () {
     showRandomQuote();
 
     $("[name=\"new-quote\"]").click(function () {
         showRandomQuote();
+    });
+
+    $("[name=\"twitter\"]").click(function () {
+        window.open(
+            shareOnTwitter(),
+            "_blank"
+        );
     });
 });
 
@@ -12,7 +17,7 @@ function showRandomQuote() {
     var randomNum = Math.floor((Math.random() * 20));
     $.getJSON("https://jsonblob.com/api/jsonBlob/d82d9345-34c0-11e7-ae4c-6beae9fe30f9", function (data) {
         $(".quote-text").fadeOut("slow", function() {
-            $(this).html(quotesHtmlCode + data.quotes[randomNum].text).fadeIn("slow");
+            $(this).html(data.quotes[randomNum].text).fadeIn("slow");
         });
         
         $(".author").fadeOut("slow", function() {
@@ -20,3 +25,10 @@ function showRandomQuote() {
         });
     });
 };
+
+function shareOnTwitter() {
+    var quote = $(".quote-text").html();
+    var author = $(".author").html();
+
+    return "https://twitter.com/intent/tweet?text=" + quote + " " + author;
+}
