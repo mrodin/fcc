22ef75a4ede3445b7b5ba3c58c5f16b72040f7e2
Martin Rodin <rodin.martin@gmail.com>
2017-08-29 23:18:46 +0200

    basic fetch API implementation

--- wikipedia-viewer/assets/scripts/script-main.js
+++ wikipedia-viewer/assets/scripts/script-main.js
@@ -1,7 +1,19 @@
 const searchBtn = document.getElementById("searchBtn");
+const searchResults = document.getElementById("searchResults");
+const url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=dog";
+// const url = "https://jsonblob.com/api/jsonBlob/d82d9345-34c0-11e7-ae4c-6beae9fe30f9";
 
 document.addEventListener("DOMContentLoaded", function () {
-    searchBtn.onclick = function() {
-        alert("test alert");
-    };
+    fetch(url)
+        .then((resp) => resp.json())
+        .then(function (data) {
+            let searchResult = data.query.search.title;
+            console.log(searchResult);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+
+    searchBtn.onclick = function () {
+    }
 });
