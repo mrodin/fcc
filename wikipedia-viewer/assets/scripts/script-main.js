512dea0d01e0df779e4678512943071f93b78d8d
Martin Rodin <rodin.martin@gmail.com>
2017-08-30 23:30:13 +0200

    call fetch on btn click

--- wikipedia-viewer/assets/scripts/script-main.js
+++ wikipedia-viewer/assets/scripts/script-main.js
@@ -3,17 +3,19 @@ const searchResults = document.getElementById("searchResults");
 const url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=dog";
 // const url = "https://jsonblob.com/api/jsonBlob/d82d9345-34c0-11e7-ae4c-6beae9fe30f9";
 
+let test = "";
+
 document.addEventListener("DOMContentLoaded", function () {
     fetch(url)
         .then((resp) => resp.json())
         .then(function (data) {
-            let searchResult = data.query.search.title;
-            console.log(searchResult);
+            test = data.query.search[0].title;
         })
         .catch(function (error) {
             console.log(error);
         })
 
     searchBtn.onclick = function () {
+        alert(test);
     }
 });
