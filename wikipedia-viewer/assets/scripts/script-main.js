4bd92b3111050e4a29df8d7ad4aa49cf6149bad8
Martin Rodin <rodin.martin@gmail.com>
2017-09-12 19:57:10 +0200

    fix fetch request

--- wikipedia-viewer/assets/scripts/script-main.js
+++ wikipedia-viewer/assets/scripts/script-main.js
@@ -1,44 +1,107 @@
 const WikiViewer = function() {
 
-  let
-    searchBtn,
-    searchResults,
-    url,
-    testString
+  let url;
 
-  const _init = function() {
-    searchBtn = document.getElementById("searchBtn");
-    searchResults = document.getElementById("searchResults");
+  const init = function() {
     url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=dog";
-    testString = "";
-
-    _getData();
-    _addEventHandlers();
+    
+    addEventHandlers();
   }
 
-  const _addEventHandlers = function() {
-    searchBtn.addEventListener("click", _getData, false);
-    searchBtn.addEventListener("click", _testFunc, false);
+  const addEventHandlers = function() {
+    document.getElementById("searchBtn").addEventListener("click", getData);
   }
 
-  const _testFunc = function() {
-    alert(testString);
-  }
-
-  const _getData = function() {
+  const getData = function() {
     fetch(url)
       .then((resp) => resp.json())
-      .then(function(data) {
-        testString = data.query.search[1].title;
-      })
-      .catch(function(error) {
-        console.log(error);
+      .then((data) => {
+        let output = "<h2>Search Results</h2>";
+        data.query.search.forEach(function(result) {
+          output += `
+                <ul class="list-group mb-3">
+                  <li>Title: ${result.title}</li>
+                  <li>Snippet: ${result.snippet}</li>
+                </ul>
+              `;
+        });
+        document.getElementById("output").innerHTML = output;
       })
   }
 
   return {
-    init: _init
+    init: init
   }
 }();
 
 WikiViewer.init();
+
+// const WikiViewer = function() {
+
+//   let
+//     searchBtn,
+//     searchResults,
+//     url,
+//     ul,
+//     testString;
+
+//   const init = function() {
+//     searchBtn = document.getElementById("searchBtn");
+//     searchResults = document.getElementById("searchResults");
+//     url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=dog";
+//     ul = document.getElementById("searchResults");
+//     testString = "";
+
+//     addEventHandlers();
+//   }
+
+//   const addEventHandlers = function() {
+//     // searchBtn.addEventListener("click", setTimeout(() => {
+//     //   getData();
+//     // }, 300), false);
+//     searchBtn.addEventListener("click", getData, false);
+//     // searchBtn.addEventListener("click", testFunc, false);
+//   }
+
+//   const testFunc = function() {
+//     setTimeout(() => {
+//       getData();
+//     }, 1000);
+//   }
+
+//   const getData = function() {
+//     fetch(url)
+//       .then((resp) => resp.json())
+//       .then(function(data) {
+//         let searchResults = data.query.search;
+//         return searchResults.map(function(result) {
+//           let
+//             li = createNode("li"),
+//             h1 = createNode("h1"),
+//             p = createNode("p");
+//           h1.innerHTML = result.title;
+//           p.innerHTML = result.snippet;
+//           append(li, h1);
+//           append(li, p);
+//           append(ul, li);
+//         })
+//       })
+//       .catch(function(error) {
+//         console.log(error);
+//       })
+//   }
+
+//   const createNode = function(element) {
+//     return document.createElement(element);
+//   }
+
+//   const append = function(parent, element) {
+//     return parent.appendChild(element);
+//   }
+
+//   return {
+//     init: init
+//   }
+// }();
+
+// WikiViewer.init();
