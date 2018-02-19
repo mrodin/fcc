27702f5441fe14107236ac7bfcb102965c63783a
Martin Rodin <martin.rodin@gmail.com>
2018-02-19 15:33:27 +0100

    refactor general code structure

--- twitch-status/assets/scripts/script-main.js
+++ twitch-status/assets/scripts/script-main.js
@@ -1,73 +1,63 @@
-const TwitchStatus = (function() {
+const baseURL = "https://wind-bow.gomix.me/twitch-api/streams/";
+const channels = "ESL_SC2";
+const allButton = document.getElementById("all-button");
+const onlineButton = document.getElementById("online-button");
+const offlineButton = document.getElementById("offline-button");
 
-  const settings = {
-    baseURL: "https://wind-bow.gomix.me/twitch-api/streams/",
-    channels: "ESL_SC2",
-    allButton: document.getElementById("all-button"),
-    onlineButton: document.getElementById("online-button"),
-    offlineButton: document.getElementById("offline-button"),
-  };
+const init = () => {
+  bindUI();
+  getJSON();
+};
 
-  const init = function() {
-    bindUI();
-    getJSON();
-  };
+const bindUI = () => {
+  allButton.addEventListener("click", function () { openTab("all-button", "all-channels"); });
+  onlineButton.addEventListener("click", function () { openTab("online-button", "online-channels"); });
+  offlineButton.addEventListener("click", function () { openTab("offline-button", "offline-channels"); });
+};
 
-  const bindUI = function() {
-    settings.allButton.addEventListener("click", function() { openTab("all-button", "all-channels"); });
-    settings.onlineButton.addEventListener("click", function() { openTab("online-button", "online-channels"); });
-    settings.offlineButton.addEventListener("click", function() { openTab("offline-button", "offline-channels"); });
-  };
+const openTab = (buttonType, channelType) => {
+  removeActiveButton();
+  hideChannels();
+  addActiveButton(buttonType);
+  showChannels(channelType);
+};
 
-  const openTab = function(buttonType, channelType) {
-    removeActiveButton();
-    hideChannels();
-    addActiveButton(buttonType);
-    showChannels(channelType);
-  };
+// Add an "active-button" class to the button that opened the tab
+const addActiveButton = (buttonType) => {
+  document.getElementById(buttonType).className += " active-button";
+};
 
-  // Add an "active-button" class to the button that opened the tab
-  const addActiveButton = function(buttonType) {
-    document.getElementById(buttonType).className += " active-button";
-  };
+// Get all elements with class="button" and remove the class "active"
+const removeActiveButton = () => {
+  let buttons = document.getElementsByClassName("button");
+  for (i = 0; i < buttons.length; i++) {
+    buttons[i].className = buttons[i].className.replace(" active-button", "");
+  }
+};
 
-  // Get all elements with class="button" and remove the class "active"
-  const removeActiveButton = function() {
-    let buttons = document.getElementsByClassName("button");
-    for (i = 0; i < buttons.length; i++) {
-      buttons[i].className = buttons[i].className.replace(" active-button", "");
-    }
-  };
+// Show channels on current tab
+const showChannels = (channelType) => {
+  document.getElementById(channelType).style.display = "grid";
+};
 
-  // Show channels on current tab
-  const showChannels = function(channelType) {
-    document.getElementById(channelType).style.display = "grid";
-  };
+// Get all elements with class="channels" and hide them
+const hideChannels = () => {
+  let channels = document.getElementsByClassName("channels");
+  for (i = 0; i < channels.length; i++) {
+    channels[i].style.display = "none";
+  }
+};
 
-  // Get all elements with class="channels" and hide them
-  const hideChannels = function() {
-    let channels = document.getElementsByClassName("channels");
-    for (i = 0; i < channels.length; i++) {
-      channels[i].style.display = "none";
-    }
-  };
+const getJSON = () => {
+  const queryURL = baseURL + channels;
+  fetch(queryURL)
+    .then((resp) => resp.json())
+    .then((data) => buildHTML(data));
+};
 
-  const getJSON = function() {
-    const queryURL = settings.baseURL + settings.channels;
-    fetch(queryURL)
-      .then((resp) => resp.json())
-      .then((data) => buildHTML(data));
-  };
+const buildHTML = (data) => {
+  let output = "test";
+  document.getElementById("test-channels").innerHTML = output;
+};
 
-  const buildHTML = function(data) {
-    let output = "test";
-    document.getElementById("test-channels").innerHTML = output;
-  };
-
-  return {
-    init: init
-  };
-
-})();
-
-TwitchStatus.init();
+init();
