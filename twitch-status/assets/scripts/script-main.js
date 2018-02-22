2be9cc90bbdb88ca17f5f72f165754fab36889c5
Martin Rodin <rodin.martin@gmail.com>
2018-02-22 14:42:00 +0100

    add channel link and status to HTML

--- twitch-status/assets/scripts/script-main.js
+++ twitch-status/assets/scripts/script-main.js
@@ -1,5 +1,5 @@
 const baseURL = "https://wind-bow.gomix.me/twitch-api/streams/";
-const channels = "ESL_SC2";
+const channels = ["ESL_SC2", "CohhCarnage", "DansGaming"];
 const allButton = document.getElementById("all-button");
 const onlineButton = document.getElementById("online-button");
 const offlineButton = document.getElementById("offline-button");
@@ -49,15 +49,31 @@ const hideChannels = () => {
 };
 
 const getJSON = () => {
-  const queryURL = baseURL + channels;
+  const queryURL = baseURL + channels[0];
   fetch(queryURL)
     .then((resp) => resp.json())
-    .then((data) => buildHTML(data));
+    .then((data) => buildChannelLists(data));
 };
 
-const buildHTML = (data) => {
-  let output = "test2";
-  document.getElementById("test-channels").innerHTML = output;
+const buildChannelLists = (data) => {
+  let onlineChannels = buildOnlineChannelHtml(data);
+  let offlineChannels = "";
+  document.getElementById("test-channels").innerHTML = onlineChannels;
+};
+
+const buildOnlineChannelHtml = (data) => {
+  let html = `
+    <li>
+        <div class="logo"><img src="${data.stream.channel.logo}" alt="${data.stream.channel.display_name}"></div>
+        <div class="name">${data.stream.channel.display_name}</div>
+        <div class="status"><i class="fa fa-bolt purple" aria-hidden="true"></i></div>
+    </li>
+  `;
+  return html;
+};
+
+const buildofflineChannelHtml = () => {
+  // TODO
 };
 
 init();
