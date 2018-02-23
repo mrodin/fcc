70f66857f08585536ac534ed41c366ede765a889
Martin Rodin <rodin.martin@gmail.com>
2018-02-23 10:15:50 +0100

    get status for all channels

--- twitch-status/assets/scripts/script-main.js
+++ twitch-status/assets/scripts/script-main.js
@@ -1,20 +1,29 @@
-const baseURL = "https://wind-bow.gomix.me/twitch-api/streams/";
-const channels = ["ESL_SC2", "CohhCarnage", "DansGaming"];
-const allButton = document.getElementById("all-button");
-const onlineButton = document.getElementById("online-button");
-const offlineButton = document.getElementById("offline-button");
+const CHANNELS_BASE_URL = "https://wind-bow.gomix.me/twitch-api/channels/";
+const STREAMS_BASE_URL = "https://wind-bow.gomix.me/twitch-api/streams/";
+const CHANNEL_LIST = ["cohhcarnage", "cretetion", "dansgaming", "esl_sc2",
+  "freecodecamp", "habathcx", "noobs2ninjas", "ogamingsc2",
+  "robotcaleb", "storbeck"];
+const ALL_BUTTON = document.getElementById("all-button");
+const ONLINE_BUTTON = document.getElementById("online-button");
+const OFFLINE_BUTTON = document.getElementById("offline-button");
 
+// Html code populated with data from Twitch API
+let onlineChannelsHtml = "";
+let offlineChannelsHtml = "";
+
+// Entry point
 const init = () => {
   bindUI();
-  getJSON();
+  buildChannelLists();
 };
 
 const bindUI = () => {
-  allButton.addEventListener("click", function () { openTab("all-button", "all-channels"); });
-  onlineButton.addEventListener("click", function () { openTab("online-button", "online-channels"); });
-  offlineButton.addEventListener("click", function () { openTab("offline-button", "offline-channels"); });
+  ALL_BUTTON.addEventListener("click", function () { openTab("all-button", "all-channels"); });
+  ONLINE_BUTTON.addEventListener("click", function () { openTab("online-button", "online-channels"); });
+  OFFLINE_BUTTON.addEventListener("click", function () { openTab("offline-button", "offline-channels"); });
 };
 
+// Gets called when user clicks on any tab control
 const openTab = (buttonType, channelType) => {
   removeActiveButton();
   hideChannels();
@@ -48,32 +57,69 @@ const hideChannels = () => {
   }
 };
 
-const getJSON = () => {
-  const queryURL = baseURL + channels[0];
-  fetch(queryURL)
-    .then((resp) => resp.json())
-    .then((data) => buildChannelLists(data));
+const buildChannelLists = () => {
+  CHANNEL_LIST.forEach((channel) => {
+    getJSON(channel, "streams");
+  });
 };
 
-const buildChannelLists = (data) => {
-  let onlineChannels = buildOnlineChannelHtml(data);
-  let offlineChannels = "";
-  document.getElementById("test-channels").innerHTML = onlineChannels;
+const getJSON = (channel, urlType) => {
+  if (urlType === "streams") {
+    let streamsFullUrl = STREAMS_BASE_URL + channel;
+    fetch(streamsFullUrl)
+      .then((resp) => resp.json())
+      .then((data) => checkChannelStatus(channel, data));
+  }
+
+  if (urlType === "channels") {
+    let channelsFullUrl = CHANNELS_BASE_URL + channel;
+    fetch(channelsFullUrl)
+      .then((resp) => resp.json())
+      .then((data) => buildOfflineChannelHtml(data));
+  }
+};
+
+// Checks if the channel is online; if the channel is offline,
+// calls getJSON to get channels response from API instead
+const checkChannelStatus = (channel, data) => {
+  if (data.stream != null) {
+    buildOnlineChannelHtml(data);
+  } else {
+    getJSON(channel, "channels");
+  }
 };
 
 const buildOnlineChannelHtml = (data) => {
+  let description = data.stream.channel.status.substring(0, 40) + "...";
   let html = `
     <li>
         <div class="logo"><img src="${data.stream.channel.logo}" alt="${data.stream.channel.display_name}"></div>
-        <div class="name">${data.stream.channel.display_name}</div>
+        <div class="channel-info">
+          <div class="name">
+            <a href="${data.stream.channel.url}" rel="nofolow" target="_blank">${data.stream.channel.display_name}</a>
+          </div>
+          <div class="description">${description}</div>
+        </div>
         <div class="status"><i class="fa fa-bolt purple" aria-hidden="true"></i></div>
     </li>
   `;
-  return html;
+  document.getElementById("test-channels").innerHTML += html;
 };
 
-const buildofflineChannelHtml = () => {
-  // TODO
+const buildOfflineChannelHtml = (data) => {
+  let html = `
+    <li>
+        <div class="logo"><img src="${data.logo}" alt="${data.display_name}"></div>
+        <div class="channel-info">
+          <div class="name">
+            <a href="${data.url}" rel="nofolow" target="_blank">${data.display_name}</a>
+          </div>
+          <div class="description"></div>
+        </div>
+        <div class="status"><i class="fa fa-bolt lightgrey" aria-hidden="true"></i></div>
+    </li>
+  `;
+  document.getElementById("test-channels").innerHTML += html;
 };
 
 init();
