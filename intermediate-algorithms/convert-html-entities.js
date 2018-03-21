0229fd9ea50ff7fee099060a76113d138edaafe9
Martin Rodin <rodin.martin@gmail.com>
2018-03-21 20:40:40 +0100

    add Convert HTML Entities challenge

---
+++ intermediate-algorithms/convert-html-entities.js
@@ -0,0 +1,11 @@
+// https://www.freecodecamp.org/challenges/convert-html-entities
+
+function convertHTML(str) {
+  const CHAR_MAP = { "&" : "&​amp;", "<" : "&​lt;", ">" : "&​gt;", '"' : '&​quot;', "'" : "&​apos;" };
+
+  
+
+  return str;
+}
+
+convertHTML("Dolce & Gabbana");
